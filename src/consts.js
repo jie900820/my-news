f124ab85e0fb27652ee53ad0b12fa5db84c228da
jie900820 <fdk8k6j8kh@privaterelay.appleid.com>
2026-05-18 20:55:46 +0800

    add: src/consts.js

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '台北拉麵地圖';
+export const SITE_DESCRIPTION = '整理台北不同風格的拉麵店，包含湯頭特色、麵體口感、排隊狀況與個人心得。';
